4f88c60be52c7b5f0e5ed8654d2c324f4cb89570
ccase629 <ccase629@uw.edu>
2022-11-13 15:01:42 -0800

    table source code

---
+++ source/table.R
@@ -0,0 +1,48 @@
+library("tidyverse")
+
+cases <- read.csv("data/choleracases.csv")
+deaths <- read.csv("data/choleradeaths.csv")
+handwash <- read.csv("data/handwash.csv")
+defecation <- read.csv("data/opendefecation.csv")
+
+fix_data <- function(data) {
+data <-
+  gather(
+    data %>% slice_tail(n = -2),
+    key = Year,
+    value = name,
+    -X
+  ) %>%
+  filter(!str_detect(Year, "\\.")) %>%
+  mutate(Year = as.integer(str_sub(Year, 2))) %>%
+  mutate(name = as.integer(name)) %>%
+  rename(Country = X)
+}
+
+cases <- cases %>% rename(cases = Number.of.reported.cases.of.cholera)
+deaths <- deaths %>% rename(deaths = Number.of.reported.deaths.from.cholera)
+
+deaths <- deaths %>% 
+  filter(deaths != "Unknown") %>%
+  mutate(deaths = as.integer(deaths))
+
+handwash <- fix_data(handwash)
+defecation <-fix_data(defecation)
+
+handwash <- handwash %>% rename(handwashing = name)
+defecation <- defecation %>% rename(defecation = name)
+
+
+data <- cases %>%
+  left_join(deaths, by = c("Country" = "Country", "Year" = "Year")) %>%
+  left_join(handwash, by = c("Country" = "Country", "Year" = "Year")) %>%
+  left_join(defecation, by = c("Country" = "Country", "Year" = "Year"))
+
+aggregate_data <- function(start, end) {
+  data %>%
+    filter(start <= Year & Year < end) %>%
+    group_by(Country) %>%
+    summarise(cases = sum(cases, na.rm = TRUE), deaths = sum(deaths, na.rm = TRUE),
+              handwashing = round(mean(handwashing, na.rm = TRUE), 1),
+              defecation = round(mean(defecation, na.rm = TRUE), 1))
+}
