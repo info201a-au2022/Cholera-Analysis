98277c7cb496a9b3c2b178e2cfd7bfe9b6b2dfa1
ccase629 <ccase629@uw.edu>
2022-11-13 13:43:59 -0800

    death rate graph

---
+++ source/deathrategraph.R
@@ -0,0 +1,28 @@
+library("tidyverse")
+library("ggpubr")
+
+cases <- read.csv("data/choleracases.csv")
+deaths <- read.csv("data/choleradeaths.csv")
+
+data <- cases %>%
+  left_join(deaths, by = c("Country" = "Country", "Year" = "Year")) %>%
+  na.omit()
+
+data_by_years <- function(start, end) {
+  data %>%
+  filter(start <= Year & Year < end) %>%
+  filter(Number.of.reported.deaths.from.cholera != "Unknown") %>%
+  group_by(Country) %>%
+  summarise(cases = sum(Number.of.reported.cases.of.cholera),
+            deaths = sum(as.integer(Number.of.reported.deaths.from.cholera))) %>%
+    return()
+  
+}
+
+graph_by_years <- function(start, end) {
+  ggplot(data = data_by_years(start, end), aes(x = cases, y = deaths)) +
+    geom_point() +
+    geom_smooth(method=lm, se=FALSE) +
+    stat_regline_equation()
+}
+             
