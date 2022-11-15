52bb27b9aff9569fe0f5332056090666ce82991c
ccase629 <ccase629@uw.edu>
2022-11-14 21:31:50 -0800

    r markdown report

--- source/Summary_information.R
+++ source/Summary_information.R
@@ -4,7 +4,38 @@
 source("../source/table.R")
 
 summary_info <- list()
-summary_info$num_observations <- nrow(my_dataframe)
-summary_info$some_max_value <- my_dataframe %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
+
+most_case_country <- summary_table %>%
+  filter(cases == max(cases)) %>%
+  pull(Country)
+
+most_death_country <- summary_table %>%
+  filter(deaths == max(deaths)) %>%
+  pull(Country)
+
+top_5_mdc <- summary_table %>%
+  arrange(desc(deaths)) %>%
+  slice_head(n = 5) %>%
+  pull(Country)
+
+top_5_mcc <- summary_table %>%
+  arrange(desc(cases)) %>%
+  slice_head(n = 5) %>%
+  pull(Country)
+
+mc_handwash <- summary_table %>%
+  filter(Country == most_case_country) %>%
+  pull(handwashing)
+
+mc_defecation <- summary_table %>%
+  filter(Country == most_case_country) %>%
+  pull(defecation)
+
+md_handwash <- summary_table %>%
+  filter(Country == most_death_country) %>%
+  pull(handwashing)
+
+md_defecation <- summary_table %>%
+  filter(Country == most_death_country) %>%
+  pull(defecation)
+
