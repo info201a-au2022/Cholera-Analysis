93f763e60340e800ae4c98e70fd7cfd1df4dd38a
Binh Kim <binhkim@Binhs-MacBook-Air.local>
2022-12-06 11:02:38 -0800

    additional small changes

--- deathsandmedicine_app/ui.R
+++ deathsandmedicine_app/ui.R
@@ -10,22 +10,24 @@
 library(shiny)
 library(plotly)
 
+
+# Tab for graph
+deaths_medicine_tab <- tabPanel("Deaths and Medicine",
+sidebarLayout(
+  sidebarPanel(
+    uiOutput("selectCountry")
+  ),
+  mainPanel(
+    plotlyOutput("countryPlot"),
+    h3("What does it mean?"),
+    p("While looking at this chart we are able to see the total deaths recorded over the span of 2007 to 2013, and the median percentage of generic medicine available for a selected country over the same span of years. From further analyzing the results, it can be seen that a country with a lower percentage of accessibility to medicine tends to have a higher overall death toll.")
+  )
+))
+
+
+
 # Defining UI
 shinyUI(fluidPage(
-  
-    # Application title
-    titlePanel("Deaths and Medicine"),
-
-    # Sidebar with select box input
-    sidebarLayout(
-        sidebarPanel(
-          uiOutput("selectCountry")
-        ),
-    # Main panel displaying chart and summary paragraph
-        mainPanel(
-          plotlyOutput("countryPlot"),
-          h3("What does it mean?"),
-          p("While looking at this chart we are able to see the total deaths recorded over the span of 2007 to 2013, and the median percentage of generic medicine available for a selected country over the same span of years. From further analyzing the results, it can be seen that a country with a lower percentage of accessibility to medicine tends to have a higher overall death toll.")
-        )
-    )
-))
+  tabsetPanel(
+  deaths_medicine_tab
+)))
