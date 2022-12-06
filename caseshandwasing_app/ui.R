d989f911cd9003046ba7b27dd53ca8669fcc4198
Marika Fong <41089568+marikafong@users.noreply.github.com>
2022-12-06 12:44:03 -0800

    documentation

--- caseshandwasing_app/ui.R
+++ caseshandwasing_app/ui.R
@@ -7,6 +7,7 @@ library(shiny)
 
 source("cases_handwashing.R")
 
+## adding theme to tab for cases and handwashing graph
 handwashing_tab <-  tabPanel(
   "Cholera Cases vs Handwashing",
   fluidPage(theme = bs_theme(bootswatch = "minty")))
@@ -32,7 +33,7 @@ country_sidebar <- sidebarPanel(
   year_select
 )
 
-
+## adding interactive plot
 handwashing_plot <- mainPanel(
   plotlyOutput(outputId = "cases_handwashing_graph")
 )
@@ -50,7 +51,7 @@ handwashing_tab <- tabPanel(
   h1("Description"),
   p("This graph shows the disribution in the number of cholera cases globally from 2000-2018 with relation to handwashing statistics. The size of each bubble represents the number of deaths per country"),
 
- ## footer
+ ## page footer
   p("INFO 201 | Autumn 2022 | Marika Fong, Carter Case, Bihn Kim, Brandon Vuong", align = "center"),
   p("Link to ", a(strong(code("INFO201-Final-Project")), href = "https://github.com/info201a-au2022/project-group-4-section-af"), align = "center")
 )
