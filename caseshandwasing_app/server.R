994fdaf89d0814aa2e48ec8873a4489f93d65ef9
Marika Fong <marika@Marikas-MacBook-Pro.local>
2022-12-05 17:33:22 -0800

    adding plotly

--- caseshandwasing_app/server.R
+++ caseshandwasing_app/server.R
@@ -3,24 +3,25 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 
-cases_df <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-af/main/data/choleracases.csv", stringsAsFactors = FALSE)
-handwashing_df <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-af/main/data/handwash.csv", stringsAsFactors =  FALSE)
-
-
 source("cases_handwashing.R")
 
-shinyServer(function(input, output){
-  output$cases_handwashing_graph <- renderPlotly({
+server <- function(input, output){
+  output$handwashing_plot <- renderPlotly({
     
     # data wrangling
-    df <- cases_df %>% 
+    plot_data <- cases_handwashing_data %>% 
       filter(country %in% input$Country) %>% 
       filter(year >= input$time[1] & year <= input$time[2])
     
+    #making chart 
+    handwashing_plot <- ggplot(data = cases_handwashing(1000, 2020), 
+                               aes(x = handwash, y = cases, size = deaths)) +
+      geom_point()
+    title = "Number of Cholera Cases and Handwashing Percentages"
     
+    return(handwashing_plot)
+  
     
-    )
   })
   
-  
-})
+}
